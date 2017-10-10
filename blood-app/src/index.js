3ac33bbb180fa9e63416105e873faeaa5cec9892
kalam-pc <ksyedharis49@gmail.com>
2017-10-10 22:58:27 +0500

    app completed

--- blood-app/src/index.js
+++ blood-app/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import { Provider } from "react-redux"
+import store from "./store"
+import * as firebase from "firebase"
+import Routes from "./Container/Router/Routes"
+import { MuiThemeProvider } from "material-ui"
+//import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCTjf_PRPYJL10bSsIFH3UdkHVPk2QbIwU",
+  authDomain: "bloodbank-876e1.firebaseapp.com",
+  databaseURL: "https://bloodbank-876e1.firebaseio.com",
+  projectId: "bloodbank-876e1",
+  storageBucket: "bloodbank-876e1.appspot.com",
+  messagingSenderId: "560849097238"
+};
+firebase.initializeApp(config);
+//injectTapEventPlugin();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <Routes />
+    </MuiThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+);
